c1f058c92d76b80d91a6722972e76ee8962c6a62
Josh Orndorff <jorndorff@pingry.org>
2016-09-23 11:11:33 -0400

    Initial Commit of starter code.

---
+++ Readme.md
@@ -0,0 +1,23 @@
+StairCases
+==========
+
+Overview
+--------
+This project is an exercise in java syntax, control, and thinking. It gets an integer input from the user and prints ascending and descending staircases of 'x' characters. The program is restricted to only use a single literal which is either the char 'x' or the String "x".
+
+For example, if the user supplies the number 3, the program will print
+*
+**
+***
+
+***
+**
+*
+
+Usage
+-----
+Compile the program with:
+`javac StairCases.java`
+
+Run the compiled bytecode with:
+`java StairCases`
---
+++ StairCases.java
@@ -0,0 +1,33 @@
+/**
+ * Write javadoc for your class here (and remove this comment, or course)
+ *
+ */
+
+public class StairCases
+{
+  /**
+   * Main method demonstrates the other wo methods by calling each of them.
+   * @param args Standard array of commandline String arguments. Not used in this program.
+   */
+
+  public static void main(String[] args)
+  {
+    //TODO change the next line so it actually gets input from the user
+    int size = 4;
+
+    printAscending(size);
+    printDescending(size);
+  }
+
+  /**
+   * Write your javadoc here
+   */
+  public static void printAscending
+  //TODO Finish this method
+
+  /**
+   * Write your javadoc here
+   */
+  public static void printDescending
+  //TODO Finish this method
+}
